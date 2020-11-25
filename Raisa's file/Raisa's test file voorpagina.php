a9269bcafcc1931eaeb096da4996317e697532d4
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-25 20:10:15 +0100

    changed: for attribute (label input) has to contain the same as the id attribute (input element)

--- Raisa's file/Raisa's test file voorpagina.php
+++ Raisa's file/Raisa's test file voorpagina.php
@@ -5,15 +5,15 @@
 <h2> Afspraak keuze </h2>
 <h2> Klant gegevens</h2>
 <form action="/index.php" method="post">
-    <label for="voornaam">Voornaam:</label><br>
+    <label for="fname">Voornaam:</label><br>
     <input type="text" id="fname" name="fname" value=""><br>
-    <label for="voornaam">Achternaam:</label><br>
+    <label for="lname">Achternaam:</label><br>
     <input type="text" id="lname" name="lname" value=""><br>
     <label for="kenteken">Kenteken:</label><br>
     <input type="text" id="kenteken" name="Kenteken" value=""><br>
     <label for="telefoonnummer">Telefoonnummer:</label><br>
     <input type="text" id="telefoonnummer" name="Telefoonnummer" value=""><br>
     <label for="email">Email:</label><br>
-    <input type="email" id="Email" name="Email" value=""><br>
+    <input type="email" id="email" name="email" value=""><br>
     <input type="submit" value="Verzend">
 </form>
