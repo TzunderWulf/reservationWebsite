70a174a1a1034307b6462034681b89c6aabd2087
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2020-11-25 17:10:08 +0100

    Create Raisa's test file.php
    Ik heb een php formulier toegevoegd

---
+++ Raisa's test file.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="">
+
+<h1> Reseverings systeem test 1</h1>
+<h2> Afspraak keuze </h2>
+<h2> Klant gegevens</h2>
+<form action="/index.php" method="post">
+    <label for="voornaam">Voornaam:</label><br>
+    <input type="text" id="fname" name="fname" value=""><br>
+    <label for="voornaam">Achternaam:</label><br>
+    <input type="text" id="lname" name="lname" value=""><br>
+    <label for="kenteken">Kenteken:</label><br>
+    <input type="text" id="kenteken" name="Kenteken" value=""><br>
+    <label for="telefoonnummer">Telefoonnummer:</label><br>
+    <input type="text" id="telefoonnummer" name="Telefoonnummer" value=""><br>
+    <label for="email">Email:</label><br>
+    <input type="email" id="Email" name="Email" value=""><br>
+    <input type="submit" value="Verzend">
+</form>
