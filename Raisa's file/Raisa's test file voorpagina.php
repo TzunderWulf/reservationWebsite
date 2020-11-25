b66e40ac2d5382f5c72f4a48753df260ef7d4582
NagalieArt <70587195+NagalieArt@users.noreply.github.com>
2020-11-25 20:22:29 +0100

    Update Raisa's test file voorpagina.php
    "het for attribute in het label element moet hetzelfde bevatten als de id attribute van het input element"
    
    Heb ik gedaan en ge-edit!

--- Raisa's file/Raisa's test file voorpagina.php
+++ Raisa's file/Raisa's test file voorpagina.php
@@ -5,15 +5,15 @@
 <h2> Afspraak keuze </h2>
 <h2> Klant gegevens</h2>
 <form action="/index.php" method="post">
-    <label for="fname">Voornaam:</label><br>
-    <input type="text" id="fname" name="fname" value=""><br>
-    <label for="lname">Achternaam:</label><br>
-    <input type="text" id="lname" name="lname" value=""><br>
+    <label for="voornaam">Voornaam:</label><br>
+    <input type="text" id="voornaam" name="voornaam" value=""><br>
+    <label for="achternaam">Achternaam:</label><br>
+    <input type="text" id="achternaam" name="Achternaam" value=""><br>
     <label for="kenteken">Kenteken:</label><br>
     <input type="text" id="kenteken" name="Kenteken" value=""><br>
     <label for="telefoonnummer">Telefoonnummer:</label><br>
     <input type="text" id="telefoonnummer" name="Telefoonnummer" value=""><br>
-    <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email" value=""><br>
+    <label for="Email">Email:></label>
+    <input type="email" id="Email" name="Email" value=""<br
     <input type="submit" value="Verzend">
 </form>
