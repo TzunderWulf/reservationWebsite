bf69366b2fd2a8868fcf7e0c7eeb41c679d22c46
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-29 00:56:35 +0100

    added: php validation and error messages, error messages ara styled in stylesheet

--- reservationPages/reservationAPK.php
+++ reservationPages/reservationAPK.php
@@ -1,5 +1,44 @@
 <?php
+$name = '';
+$phoneNumber = '';
+$email = '';
+$licensePlate = '';
 
+$nameErr = '';
+$emailErr = '';
+$licensePlateErr = '';
+
+if (isset($_POST['submit'])) {
+    $validForm = true; // boolean to check if form is valid, changes based on if field is empty
+
+    // validation for the name input
+    if (!isset($_POST['name']) || $_POST['name'] === '') {
+        $validForm = false;
+        $nameErr = 'Dit veld is verplicht.';
+    } else {
+        $name = htmlspecialchars($_POST['name']);
+    }
+    // validation for the email input
+    if (!isset($_POST['email']) || $_POST['email'] === '') {
+        $validForm = false;
+        $emailErr = 'Dit veld is verplicht.';
+    } else {
+        $email = htmlspecialchars($_POST['email']);
+    }
+    // validation for the license plate input
+    if (!isset($_POST['licensePlate']) || $_POST['licensePlate'] === '') {
+        $validForm = false;
+        $licensePlateErr = 'Dit veld is verplicht.';
+    } else {
+        $licensePlate = htmlspecialchars($_POST['licensePlate']);
+    }
+    $phoneNumber = $_POST['phoneNumber'];
+
+    // if the entire form is valid sent user to confirmation page
+    if ($validForm) {
+        header('Location: ../confirmation.php');
+    }
+}
 ?>
 
 <!doctype html>
@@ -16,30 +55,39 @@
 
         <form action="" method="post">
             <!-- input for name !-->
-            <label for="voornaam">Naam*: </label>
-            <input type="text" id="voornaam" name="voornaam"><br>
+            <label for="naam">Naam*: </label>
+            <input type="text" id="naam" name="name" value="<?=
+            htmlspecialchars($name, ENT_QUOTES);
+            ?>">
+            <p class="error"><?=$nameErr;?></p><br>
 
             <!-- input for phone number !-->
             <label for="telefoonnummer">Telefoonnummer: </label>
-            <input type="text" id="telefoonnummer" name="telefoonnummer" maxlength="11" placeholder="06-12345678"><br>
+            <input type="text" id="telefoonnummer" name="phoneNumber" maxlength="11" placeholder="06-12345678" value="<?=
+            htmlspecialchars($phoneNumber, ENT_QUOTES);
+            ?>"><br>
 
             <!-- input for email address !-->
             <label for="email">Emailadres*: </label>
-            <input type="email" id="email" name="email" placeholder="example@example.nl"><br>
+            <input type="email" id="email" name="email" placeholder="example@example.nl" value="<?=
+            htmlspecialchars($email, ENT_QUOTES);?>">
+            <p class="error"><?=$emailErr;?></p><br>
 
             <!-- input for license plate !-->
             <label for="kenteken">Kenteken*: </label>
-            <input type="text" id="kenteken" name="kenteken" maxlength="8" placeholder="AB-C3D-5"><br>
+            <input type="text" id="kenteken" name="licensePlate" maxlength="8" placeholder="AB-C3D-5" value="<?=
+            htmlspecialchars($licensePlate, ENT_QUOTES);
+            ?>">
+            <p class="error"><?=$licensePlateErr;?></p><br>
+
+            <h3>Kies hieronder een datum voor de resevering.</h3>
+
+            <!-- agenda with possible times !-->
+
+            <!-- resetting and submitting the form to validate !-->
+            <!-- in case the form fails to validate, user is send back here and error is shown !-->
+            <input type="reset" name="reset" value="Reset">
+            <input type="submit" name="submit" value="Bevestigen">
         </form>
-
-        <h3>Kies hieronder een datum voor de resevering.</h3>
-
-        <!-- agenda with possible times !-->
-
-        <!-- resetting and submitting the form to validate !-->
-        <!-- in case the form fails to validate, user is send back here and error is shown !-->
-        <input type="reset" name="reset" value="Reset">
-        <input type="submit" name="submit" value="Bevestigen">
-
     </body>
 </html>
--- reservationPages/reservationLoancar.php
+++ reservationPages/reservationLoancar.php
@@ -1,6 +1,46 @@
 <?php
+$name = '';
+$phoneNumber = '';
+$email = '';
+$carChoice = '';
 
+$nameErr = '';
+$emailErr = '';
+$carChoiceErr = '';
+
+if (isset($_POST['submit'])) {
+    $validForm = true; // boolean to check if form is valid, changes based on if field is empty
+
+    // validation for the name input
+    if (!isset($_POST['name']) || $_POST['name'] === '') {
+        $validForm = false;
+        $nameErr = 'Dit veld is verplicht.';
+    } else {
+        $name = htmlspecialchars($_POST['name']);
+    }
+    // validation for the email input
+    if (!isset($_POST['email']) || $_POST['email'] === '') {
+        $validForm = false;
+        $emailErr = 'Dit veld is verplicht.';
+    } else {
+        $email = htmlspecialchars($_POST['email']);
+    }
+    // validation for the license plate input
+    if (!isset($_POST['carChoice']) || $_POST['carChoice'] === '') {
+        $validForm = false;
+        $carChoiceErr = 'Dit veld is verplicht.';
+    } else {
+        $carChoice = htmlspecialchars($_POST['carChoice']);
+    }
+    $phoneNumber = $_POST['phoneNumber'];
+
+    // if the entire form is valid sent user to confirmation page
+    if ($validForm) {
+        header('Location: ../confirmation.php');
+    }
+}
 ?>
+
 
 <!doctype html>
 <html lang="nl">
@@ -17,32 +57,43 @@
         <form action="" method="post">
             <!-- input for name !-->
             <label for="voornaam">Naam*: </label>
-            <input type="text" id="voornaam" name="voornaam"><br>
+            <input type="text" id="voornaam" name="name"value="<?=
+            htmlspecialchars($name, ENT_QUOTES);
+            ?>">
+            <p class="error"><?=$nameErr;?></p><br>
 
             <!-- input for phone number !-->
             <label for="telefoonnummer">Telefoonnummer: </label>
-            <input type="text" id="telefoonnummer" name="telefoonnummer" maxlength="11" placeholder="06-12345678"><br>
+            <input type="text" id="telefoonnummer" name="phoneNumber" maxlength="11" placeholder="06-12345678"><br>
 
             <!-- input for email address !-->
             <label for="email">Emailadres*: </label>
-            <input type="email" id="email" name="email" placeholder="example@example.nl"><br>
+            <input type="email" id="email" name="email" placeholder="example@example.nl"value="<?=
+            htmlspecialchars($email, ENT_QUOTES);
+            ?>">
+            <p class="error"><?=$emailErr;?></p><br>
 
             <!-- input for license plate !-->
-            <p>Kies een van de twee auto's*: </p>
-            <input type="radio" id="keuze_auto" name="keuze_auto" value="Auto 1">
-            <label for="keuze_auto">Auto 1</label>
-            <input type="radio" id="keuze_auto" name="keuze_auto" value="Auto 2">
-            <label for="keuze_auto">Auto 2</label><br>
+            <label for="autoKeuze">Kies een van de twee auto's*: </label>
+            <input type="radio" id="autoKeuze" name="carChoice" value="car1"<?php
+                if ($carChoice == 'car1') {
+                    echo ' checked';
+                }?>>Auto 1
+            <input type="radio" id="autoKeuze" name="carChoice" value="car2"<?php
+                if ($carChoice == 'car2') {
+                echo ' checked';
+                }?>>Auto 2
+            <p class="error"><?=$carChoiceErr;?></p>
+
+            <h3>Selecteer hieronder de periode dat u de auto wilt lenen.</h3>
+
+            <!-- agenda with possible times !-->
+
+            <!-- resetting and submitting the form to validate !-->
+            <!-- in case the form fails to validate, user is send back here and error is shown !-->
+            <input type="reset" name="reset" value="Reset">
+            <input type="submit" name="submit" value="Bevestigen">
         </form>
-
-        <h3>Selecteer hieronder de periode dat u de auto wilt lenen.</h3>
-
-        <!-- agenda with possible times !-->
-
-        <!-- resetting and submitting the form to validate !-->
-        <!-- in case the form fails to validate, user is send back here and error is shown !-->
-        <input type="reset" name="reset" value="Reset">
-        <input type="submit" name="submit" value="Bevestigen">
 
     </body>
 </html>
