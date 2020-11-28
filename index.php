2afcf70b434e7bd89f30fbcde8787225e8d8a96a
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-28 01:13:33 +0100

    changed: links now work to the last two reservations

--- index.php
+++ index.php
@@ -4,20 +4,18 @@
 <!doctype html>
 <html lang="nl">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="stylesheet.css">
         <title>Afspraak maken</title>
     </head>
+
     <body>
+        <a href="https://garagenieuwrijswijk.nl">Terug naar de website</a>
         <h1>Afspraak maken</h1>
         <h3>Welkom! <br> Waarvoor wilt u een afspraak maken?</h3>
 
         <h3>Onderhoud</h3>
         <p>Auto's moeten nou eenmaal onderhouden worden. Daar kunt u bij ons voor terrecht.</p>
-        <a href="">Maak nu een afspraak</a>
+        <a href="reservationUpkeep.php">Maak nu een afspraak</a>
 
         <h3>Reperatie</h3>
         <p>Soms gaat er iets kapot, gelukkig kunt u bij ons daarvoor terrecht.</p>
@@ -25,10 +23,10 @@
 
         <h3>APK keuring</h3>
         <p>APK keuringen zijn verplicht om eens in het jaar te doen. Hier hebben wij gelukkig ook een optie voor.</p>
-        <a href="">Maak nu een afspraak</a>
+        <a href="reservationAPK.php">Maak nu een afspraak</a>
 
         <h3>Auto lenen</h3>
         <p>Tijdelijk een auto nodig? Maak gerust een afspraak en hij staat voor u klaar.</p>
-        <a href="">Maak nu een afspraak</a>
+        <a href="reservationLoancar.php">Maak nu een afspraak</a>
     </body>
 </html>
