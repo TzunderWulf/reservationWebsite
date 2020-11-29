1f3a99719b8955955d77e77e1bb3823d17b8e4ac
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-11-29 02:03:46 +0100

    changed text into placeholder text

--- confirmation.php
+++ confirmation.php
@@ -16,12 +16,6 @@
         <h1>Afspraak bevestigd</h1>
         <h3>Er is een afspraak gemaakt voor [afspraak] op [date]</h3>
         <h3>De volgende informatie is doorgegeven (deze informatie wordt ook naar uw verzonden per email): </h3>
-        <ul>
-            <li>Naam: <?php echo $_POST['Voornaam'] . " " . $_POST['Achternaam'];?></li>
-            <li>Telefoonnummer: <?php echo $_POST['Telefoonnummer'];?></li>
-            <li>Emailadres: <?php echo $_POST['Email'];?></li>
-            <li>Kenteken: <?php echo $_POST['Kenteken'];?></li>
-            <li>Werkzaamheden: <?php echo $_POST['Werkzaamheden'];?></li>
-        </ul>
+        <p>Informatie</p>
     </body>
 </html>
