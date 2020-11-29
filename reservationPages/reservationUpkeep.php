796b445c5cd0960d1744057de37dbbb32374ed23
TzunderWulf <svandenbos@tzunder.nl>
2020-11-29 15:37:16 +0100

    checked all the code, fixed form validation (commented on all things changed)

--- reservationPages/reservationUpkeep.php
+++ reservationPages/reservationUpkeep.php
@@ -1,4 +1,7 @@
 <?php
+// S: ik heb overal $_POST['key'] aangepast zodat 'key' overeenkomt met de name value
+// van de inputs (hoofdlettergevoelig btw)
+
 #variabelen
 $naam = '';
 $telefoonnummer = '';
@@ -6,6 +9,7 @@ $email = '';
 $kenteken= '';
 $omschrijvingOnderhoud = '';
 
+// Error variabelen
 $naamErr = '';
 $emailErr = '';
 $kentekenErr = '';
@@ -31,19 +35,22 @@ if (isset($_POST['submit'])) {
     // validatie voor kenteken
     if (!isset($_POST['kenteken']) || $_POST['kenteken'] === '') {
         $validForm = false;
-        $kenteken = 'Dit veld is verplicht.';
+        $kentekenErr = 'Dit veld is verplicht.';
+        // S: was $kenteken, moest $kentekenErr
     } else {
         $kenteken = htmlspecialchars($_POST['kenteken']);
     }
     // validatie voor onderhoud
-    if (!isset($_POST['omschrijving werkzaamheden']) || $_POST['omschrijving werkzaamheden'] === '') {
+    if (!isset($_POST['omschrijvingOnderhoud']) || $_POST['omschrijvingOnderhoud'] === '') {
         $validForm = false;
-        $omschrijvingWerkzaamheden = 'Dit veld is verplicht.';
+        $omschrijvingOnderhoudErr = 'Dit veld is verplicht.';
+        // S: was $omschrijvingOnderhoud, moest $omschrijvingOnderhoudErr
     } else {
-        $omschrijvingWerkzaamheden = htmlspecialchars($_POST['omschrijving werkzaamheden']);
+        $omschrijvingOnderhoud = htmlspecialchars($_POST['omschrijvingOnderhoud']);
     }
     //Telefoonummer
-    $telefoonnummer = $_POST['telefoonNummer'];
+    $telefoonnummer = $_POST['telefoonnummer']; // S: was $_POST['telefoonNummer'] moest _POST['telefoonnummer'] zijn
+    // (overeenkomen met name van input)
 
     // if the entire form is valid sent user to confirmation page
     if ($validForm) {
@@ -67,16 +74,13 @@ if (isset($_POST['submit'])) {
 <form action="" method="post">
     <!-- input voor naam !-->
     <label for="naam">Naam*: </label>
-    <input type="text" id="naam" name="Naam" value="<?=
-    htmlspecialchars($naam, ENT_QUOTES);
-    ?>">
+    <input type="text" id="naam" name="naam" value="<?=htmlspecialchars($naam, ENT_QUOTES);?>">
     <p class="error"><?=$naamErr;?></p><br>
 
     <!-- input voor telefoon nummer !-->
     <label for="telefoonnummer">Telefoonnummer: </label>
-    <input type="text" id="telefoonnummer" name="Telefoonnummer" maxlength="11" placeholder="06-12345678" value="<?=
-    htmlspecialchars($telefoonnummer, ENT_QUOTES);
-    ?>"><br>
+    <input type="text" id="telefoonnummer" name="telefoonnummer" maxlength="11" placeholder="06-12345678"
+        value="<?=htmlspecialchars($telefoonnummer, ENT_QUOTES);?>"><br>
 
     <!-- input voor email address !-->
     <label for="email">Emailadres*: </label>
@@ -86,14 +90,16 @@ if (isset($_POST['submit'])) {
 
     <!-- input voor kenteken !-->
     <label for="kenteken">Kenteken*: </label>
-    <input type="text" id="kenteken" name="Kenteken" maxlength="8" placeholder="AB-C3D-5" value="<?=
+    <input type="text" id="kenteken" name="kenteken" maxlength="8" placeholder="AB-C3D-5" value="<?=
     htmlspecialchars($kenteken, ENT_QUOTES);
     ?>">
     <p class="error"><?=$kentekenErr;?></p><br>
 
+    <!-- Hiervoor kijken naar textarea's !-->
+    <!-- https://www.w3schools.com/tags/tag_textarea.asp en het stylen: https://www.w3schools.com/css/css_form.asp !-->
     <!-- input voor Omschrijving type onderhoudt !-->
     <label for = "omschrijving onderhoud" >Omschrijving wat voor type onderhoud</label>
-    <input type="text" id="omschrijving onderhoud" name="Omschrijving onderhoud" maxlength="1000" placeholder="Voer hierin wat uw problemen zijn met u auto..." value="<?=
+    <input type="text" id="omschrijving onderhoud" name="omschrijvingOnderhoud" maxlength="1000" placeholder="Voer hierin wat uw problemen zijn met u auto..." value="<?=
     htmlspecialchars($omschrijvingOnderhoud, ENT_QUOTES);
     ?>">
     <p class="error"><?=$omschrijvingOnderhoudErr;?></p><br>
