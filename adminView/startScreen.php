0fbf78ff269cfb3e1cb23c1c8c33b5498e271aa6
TzunderWulf <69017007+TzunderWulf@users.noreply.github.com>
2020-12-01 21:40:10 +0100

    added: login and startscreen (for client), can get data from database

---
+++ adminView/startScreen.php
@@ -0,0 +1,91 @@
+<?php
+    require_once('../includes/connect.php');
+
+    // get the result set from the database with query
+    $queryRepair = "SELECT orderId,name,phoneNumber,email,licensePlate,description FROM reservations WHERE meeting = 'Onderhoud'";
+    $resultRepair = mysqli_query($db, $queryRepair);
+
+    $reservationsMain = [];
+    // loop trough with while
+    while($row = mysqli_fetch_assoc($resultRepair)) {
+        $reservationsMain[] = $row;
+        break;
+    }
+    $reservationsAPK = [];
+
+    $queryAPK = "SELECT orderId,name,phoneNumber,email,licensePlate FROM reservations WHERE meeting = 'APK'";
+    $resultAPK = mysqli_query($db, $queryAPK);
+    // loop trough with while
+    while($row = mysqli_fetch_assoc($resultAPK)) {
+        $reservationsAPK[] = $row;
+        break;
+    }
+    // close connection
+    $db->close();
+?>
+
+<!doctype html>
+<html lang="nl">
+<head>
+    <title>Homescreen</title>
+    <link rel="stylesheet" href="../stylesheet.css">
+</head>
+
+<body>
+    <h1>Welkom!</h1>
+
+    <h3>Alle onderhoud afspraken</h3>
+    <table>
+        <thead>
+        <tr>
+            <th>order id*</th>
+            <th>naam*</th>
+            <th>telefoonnummer</th>
+            <th>email*</th>
+            <th>kenteken</th>
+            <th>beschrijving</th>
+            <th><?php if (isset ($row['carChoice'])) {?>auto keuze<?php } ?></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($resultRepair as $row) { ?>
+            <tr>
+                <td><?php print_r($row['orderId']);?></td>
+                <td><?php print_r($row['name']);?></td>
+                <td><?php print_r($row['phoneNumber']);?></td>
+                <td><?php print_r($row['email']);?></td>
+                <td><?php print_r($row['licensePlate']);?></td>
+                <td><?php print_r($row['description']);?></td>
+                <td><a href="detail.php?index=<?=$row['orderId']?>">Details</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+
+    <h3>Alle APK afspraken</h3>
+    <table>
+        <thead>
+        <tr>
+            <th>order id*</th>
+            <th>naam*</th>
+            <th>telefoonnummer</th>
+            <th>email*</th>
+            <th>kenteken</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($resultAPK as $row) { ?>
+            <tr>
+                <td><?php print_r($row['orderId']);?></td>
+                <td><?php print_r($row['name']);?></td>
+                <td><?php print_r($row['phoneNumber']);?></td>
+                <td><?php print_r($row['email']);?></td>
+                <td><?php print_r($row['licensePlate']);?></td>
+                <td><a href="detail.php?index=<?=$row['orderId']?>">Details</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
+
