8505addb25c220c4146fff9a8eed1c99e88951db
TzunderWulf <svandenbos@tzunder.nl>
2020-12-11 13:53:32 +0100

    added: validation for license plates (requires composer)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5641380d71b62216b44136de21d4feabbff84865',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5641380d71b62216b44136de21d4feabbff84865',
+    ),
+    'divtag-nl/license-plate' => 
+    array (
+      'pretty_version' => 'v1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8de254feaddb0db53adfb90f11886efc385b767e',
+    ),
+  ),
+);
