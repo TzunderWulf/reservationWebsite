aa770bad3d84716b221ad335f9667a5ee9a67dd2
TzunderWulf <svandenbos@tzunder.nl>
2020-12-11 20:52:47 +0100

    cleaned up the index page

--- index.php
+++ index.php
@@ -1,39 +1,73 @@
-<?php
-?>
-
 <!doctype html>
 <html lang="nl">
     <head>
+        <title></title>
         <link rel="stylesheet" href="styles/stylesheet.css">
-        <title>Afspraak maken</title>
+
+        <!-- Google Font-->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&display=swap" rel="stylesheet">
     </head>
 
-    <img id=header src="https://garagenieuwrijswijk.nl/wp-content/uploads/2014/01/cropped-header45.png" alt="Garage nieuw rijswijk">
-
     <body>
-    <a href="https://garagenieuwrijswijk.nl">
-        <button>Terug naar de website</button>
-    </a>
+        <header>
+            <img id="header" src="https://garagenieuwrijswijk.nl/wp-content/uploads/2014/01/cropped-header45.png" alt="Garage nieuw rijswijk">
+        </header>
 
-        <h1>Afspraak maken</h1>
-        <h3>Welkom! <br> Waarvoor wilt u een afspraak maken?</h3>
+        <main>
+            <section>
+                <!-- back to website -->
+                <a href="https://garagenieuwrijswijk.nl">
+                    <button>Terug naar de website</button>
+                </a>
+            </section>
 
-        <h3>Onderhoud</h3>
-        <p>Auto's moeten nou eenmaal onderhouden worden. Daar kunt u bij ons voor terrecht.</p>
-    <a href="reservationPages/reservationUpkeep.php"><button>Maak nu een afspraak</button></a>
+                <!-- introduction -->
+            <section>
+                <h1>Welkom!</h1>
+                <h1>Waarvoor komt u een afspraak maken?</h1>
+            </section>
 
-        <h3>Reperatie</h3>
-        <p>Soms gaat er iets kapot, gelukkig kunt u bij ons daarvoor terrecht.</p>
-    <a href="reservationPages/reservationRepair.php"><button>Maak nu een afspraak</button></a>
+            <section>
+                <!-- types of reservations -->
+                <div>
+                    <h3>Onderhoud</h3>
+                    <p>Auto's moeten nou eenmaal onderhouden worden. Daar kunt u bij ons voor terrecht.</p>
+                    <a href="reservation/maintenance.php">
+                        <button>Terug naar de website</button>
+                    </a>
+                </div>
 
-        <h3>APK keuring</h3>
-        <p>APK keuringen zijn verplicht om eens in het jaar te doen. Hier hebben wij gelukkig ook een optie voor.</p>
-    <a href="reservationPages/reservationAPK.php"><button>Maak nu een afspraak</button></a>
+                <div>
+                    <h3>Reperatie</h3>
+                    <p>Soms gaat er iets kapot, gelukkig kunt u bij ons daarvoor terrecht.</p>
+                    <a href="reservation/repair.php">
+                        <button>Terug naar de website</button>
+                    </a>
+                </div>
 
-        <h3>Auto lenen</h3>
-        <p>Tijdelijk een auto nodig? Maak gerust een afspraak en hij staat voor u klaar.</p>
-    <a href="reservationPages/reservationLoancar.php"><button>Maak nu een afspraak</button></a>
+                <div>
+                    <h3>APK</h3>
+                    <p>APK keuringen zijn verplicht om eens in het jaar te doen. Hier hebben wij gelukkig ook een optie voor.</p>
+                    <a href="reservation/apk.php">
+                        <button>Terug naar de website</button>
+                    </a>
+                </div>
+
+                <div>
+                    <h3>Auto uitlenen</h3>
+                    <p>Tijdelijk een auto nodig, door reperatie of onderhoud? Maak gerust een afspraak en hij staat voor u klaar.</p>
+                    <a href="reservation/car.php">
+                        <button>Terug naar de website</button>
+                    </a>
+                </div>
+            </section>
+        </main>
+
+        <!--
+        <footer>
+            <p>Made with the power of PHP</p>
+        </footer>
+        -->
     </body>
 </html>
