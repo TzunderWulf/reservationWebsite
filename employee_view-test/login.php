0ee4b8e7eea292f7b05b9b87545cf60b44df90e4
TzunderWulf <svandenbos@tzunder.nl>
2020-12-22 00:46:48 +0100

    added: general grid for the login page

--- employee_view-test/login.php
+++ employee_view-test/login.php
@@ -6,21 +6,31 @@ require_once("../includes/validation_login.php")
     <html lang="nl">
         <head>
             <title>Inloggen</title>
-            <link rel="stylesheet" href="../styles/stylesheet_employee.css">
+            <link rel="stylesheet" href="../styles/stylesheet-login.css">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
             <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&display=swap" rel="stylesheet">
         </head>
 
-        <body>
-            <h1>Welkom, log hieronder in.</h1>
+        <body class="container">
+        <div class="item-a">
+            <img src="https://garagenieuwrijswijk.nl/wp-content/uploads/2014/01/cropped-header45.png" alt="header image">
+        </div>
+
+        <div class="item-b">
             <form action="" method="post">
-                <div>
-                <label for="gebruikersnaam">Gebruikersnaam: </label>
+                <h1>Welkom, log hieronder in.</h1>
+
+                <div class="form-field">
+                <label for="gebruikersnaam">Gebruikersnaam:</label>
                 <input type="text" id="gebruikersnaam" name="username" value="<?=$username?>">
                 <p class="error-message"><?=$usernameErr?></p>
                 </div>
 
-                <div>
-                <label for="wachtwoord">Wachtwoord: </label>
+                <!-- adding capslock detection? -->
+
+                <div class="form-field">
+                <label for="wachtwoord">Wachtwoord:</label>
                 <input type="password" id="wachtwoord" name="password">
                 <p class="error-message"><?=$passwordErr?></p>
                 </div>
@@ -29,5 +39,10 @@ require_once("../includes/validation_login.php")
                 <input type="submit" name="submit" value="Inloggen">
                 </div>
             </form>
+        </div>
+
+        <div class="item-c">
+            <p>Aan dit systeem kunnen geen rechten worden voorgeleend. <br> Het systeem is op dit moment nog in de bouw.</p>
+        </div>
         </body>
     </html>
