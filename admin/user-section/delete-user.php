f180939b48c0d81e2bd4fae9697b3d3b6f8630cd
TzunderWulf <svandenbos@tzunder.nl>
2020-12-24 23:16:26 +0100

    added: list of users and ability to delete users

--- admin/user-section/delete-user.php
+++ admin/user-section/delete-user.php
@@ -7,3 +7,80 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     header('Location: ../index.php');
     exit();
 }
+
+require_once '../../includes/config.php'; // connect to database
+if (isset($_POST['submit'])) {
+    $query = "DELETE FROM users WHERE id = " . mysqli_escape_string($db, $_POST['id']);
+    $result = mysqli_query($db, $query)
+        or die ("Something went wrong. " . $query);
+    mysqli_close($db); // close connection
+
+    header('Location: overview-user.php'); // redirect to user overview
+    exit();
+} elseif (isset($_GET['id'])) {
+    $userId = $_GET['id'];
+    $query = "SELECT id, username FROM users WHERE id = " . mysqli_escape_string($db, $userId);
+    $result = mysqli_query($db, $query)
+        or die ("Something went wrong. " . $query);
+
+    // check if user exists in database, if not redirect back to overview
+    if (mysqli_num_rows($result) == 1) {
+        $user = mysqli_fetch_assoc($result);
+    } else {
+        header('Location: overview-user.php');
+        exit();
+    }
+} else {
+    header('Location: overview-user.php');
+    exit();
+}
+
+?>
+
+<!doctype html>
+<html lang="nl">
+<head>
+    <title>Gebruiker verwijderen</title>
+    <link rel="stylesheet" href="../../styles/stylesheet-admin.css">
+
+    <!-- Google Font -->
+</head>
+
+<body class="container">
+    <div class="item-a">
+        <img src="../../images/header.png" alt="header image">
+    </div>
+
+    <div class="user">
+        <h2>Welkom, <?=$_SESSION['username']?>!</h2>
+        <h3><?=date('d-m-Y H:i')?></h3>
+        <?php if ($_SESSION['admin'] === 1) { ?>
+            <a class="link-button" href="overview-user.php">
+                <div class="user-button">Gebruikers</div>
+            </a>
+        <?php } ?>
+        <a class="link-button" href="">
+            <div class="user-button">Hulp</div>
+        </a>
+        <a class="link-button" href="../logout.php">
+            <div class="user-button">Uitloggen</div>
+        </a>
+    </div>
+
+    <div class="item-b">
+        <form id="delete" action="" method="post">
+            <h1>Wilt u <?= $user['username'] ?> verwijderen?</h1>
+
+            <input type="hidden" name="id" value="<?= $user['id'] ?>"/>
+            <div>
+            <input type="submit" name="submit" value="Verwijderen">
+            </div>
+            <a class="link-button" href="overview-user.php">
+                <div class="button">
+                    <h3>Terug</h3>
+                </div>
+            </a>
+        </form>
+    </div>
+</body>
+</html>
--- admin/user-section/overview-user.php
+++ admin/user-section/overview-user.php
@@ -1,9 +1,108 @@
 <?php
 session_start();
-if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     header('Location: ../login.php');
     exit();
 } elseif ($_SESSION['admin'] != 1) {
     header('Location: ../index.php');
     exit();
 }
+
+require_once '../../includes/config.php';
+
+$query = "SELECT id, username, admin FROM users";
+$result = mysqli_query($db, $query);
+
+$users = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $users[] = $row;
+    break;
+}
+?>
+
+<!doctype>
+<html lang="nl">
+<head>
+    <title>Gebruikers</title>
+    <link rel="stylesheet" href="../../styles/stylesheet-admin.css">
+
+    <!-- Google Font -->
+</head>
+
+<body class="container">
+    <div class="item-a">
+        <img src="../../images/header.png" alt="header image">
+    </div>
+
+    <div class="user">
+        <h2>Welkom, <?=$_SESSION['username']?>!</h2>
+        <h3><?=date('d-m-Y H:i')?></h3>
+        <?php if ($_SESSION['admin'] === 1) { ?>
+            <a class="link-button" href="overview-user.php">
+                <div class="user-button">Gebruikers</div>
+            </a>
+        <?php } ?>
+        <a class="link-button" href="">
+            <div class="user-button">Hulp</div>
+        </a>
+        <a class="link-button" href="../logout.php">
+            <div class="user-button">Uitloggen</div>
+        </a>
+    </div>
+
+    <div class="item-c">
+        <div id="options">
+            <a class="link-button" href="create-user.php">
+                <div class="button">
+                    <h3>Gebruiker toevoegen</h3>
+                </div>
+            </a>
+            <a class="link-button" href="../index.php">
+                <div class="button">
+                    <h3>Terug</h3>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="item-b">
+        <div class="row">
+            <div class="column">
+                <p>ID</p>
+            </div>
+            <div class="column">
+                <p>Gebruikersnaam</p>
+            </div>
+            <div class="column">
+                <p>Admin</p>
+            </div>
+            <div class="column"></div>
+            <div class="column"></div>
+        </div>
+
+        <?php foreach ($result as $row => $user) { ?>
+            <div class="row">
+                <div class="column">
+                    <p><?=$user['id']?></p>
+                </div>
+                <div class="column">
+                    <p><?=$user['username']?></p>
+                </div>
+                <div class="column">
+                    <p><?= $user['admin'] == 1 ? "Ja" : "Nee"?></p>
+                </div>
+                <div class="column">
+                    <a href="edit-user.php?id=<?=$user['id']?>">
+                        <p>Aanpassen</p>
+                    </a>
+                </div>
+                <div class="column">
+                    <a href="delete-user.php?id=<?=$user['id']?>">
+                        <p>Verwijderen</p>
+                    </a>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+    </body>
+</html>
