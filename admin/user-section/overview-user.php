c00a2cffbcafcbe63ffa62166438698fcb55ef17
TzunderWulf <svandenbos@tzunder.nl>
2020-12-28 20:19:11 +0100

    added fonts and footer element

--- admin/user-section/overview-user.php
+++ admin/user-section/overview-user.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+require_once '../../includes/config.php'; // to connect to database
+
+// make sure user is logged in
 if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     header('Location: ../login.php');
     exit();
@@ -8,8 +11,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     exit();
 }
 
-require_once '../../includes/config.php';
-
+// getting all users from database
 $query = "SELECT id, username, admin FROM users";
 $result = mysqli_query($db, $query);
 
@@ -23,49 +25,43 @@ while ($row = mysqli_fetch_assoc($result)) {
 <!doctype>
 <html lang="nl">
 <head>
+
     <title>Gebruikers</title>
     <link rel="stylesheet" href="../../styles/stylesheet-admin.css">
 
     <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&display=swap" rel="stylesheet">
+
 </head>
 
 <body class="container">
-    <div class="item-a">
+
+    <header class="item-a">
         <img src="../../images/header.png" alt="header image">
-    </div>
+    </header>
 
     <div class="user">
         <h2>Welkom, <?=$_SESSION['username']?>!</h2>
         <h3><?=date('d-m-Y H:i')?></h3>
+
+        <!-- if user is an admin show button for user overview -->
         <?php if ($_SESSION['admin'] === 1) { ?>
             <a class="link-button" href="overview-user.php">
                 <div class="user-button">Gebruikers</div>
             </a>
         <?php } ?>
+
         <a class="link-button" href="">
             <div class="user-button">Hulp</div>
         </a>
+
         <a class="link-button" href="../logout.php">
             <div class="user-button">Uitloggen</div>
         </a>
     </div>
 
-    <div class="item-c">
-        <div id="options">
-            <a class="link-button" href="create-user.php">
-                <div class="button">
-                    <h3>Gebruiker toevoegen</h3>
-                </div>
-            </a>
-            <a class="link-button" href="../index.php">
-                <div class="button">
-                    <h3>Terug</h3>
-                </div>
-            </a>
-        </div>
-    </div>
-
-    <div class="item-b">
+    <main class="item-b">
         <div class="row">
             <div class="column">
                 <p>ID</p>
@@ -103,6 +99,30 @@ while ($row = mysqli_fetch_assoc($result)) {
                 </div>
             </div>
         <?php } ?>
+    </main>
+
+    <!-- sidebar -->
+    <div class="item-c">
+        <div id="options">
+            <a class="link-button" href="create-user.php">
+                <div class="button">
+                    <h3>Gebruiker toevoegen</h3>
+                </div>
+            </a>
+            <a class="link-button" href="../index.php">
+                <div class="button">
+                    <h3>Terug</h3>
+                </div>
+            </a>
+        </div>
     </div>
+
+    <footer class="item-d">
+        <p>
+            Aan dit systeem kunnen geen rechten worden voorgeleend. <br>
+            Het systeem is op dit moment nog in de bouw.
+        </p>
+    </footer>
+
     </body>
 </html>
